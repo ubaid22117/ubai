096ec1a65f5651ada4e393ab16e1872df6fcd33e
ubaid22117 <153904059+ubaid22117@users.noreply.github.com>
2024-02-22 03:42:22 -0800

    javascrpt project

---
+++ .js
@@ -0,0 +1,6 @@
+var num1 = 10;
+var num2 = 5; 
+console.log("Addition: " + (num1 + num2));
+console.log("Subtraction: " + (num1 - num2));
+console.log("Multiplication: " + (num1 * num2));
+console.log("Division: " + (num1 / num2));
--- project.js
+++ project.js
@@ -1,37 +1,36 @@
-function runJavaScriptCode() {
-    // Task 1: Alerts
-    alert("Welcome to the JavaScript Basics Assignment!");
+// Task 1: Alerts
+alert("Welcome to the JavaScript Basics Assignment!");
+// Task 2: Variables for Strings
+var favoriteFood = "Pizza";
+console.log("My favorite food is: " + favoriteFood);
 
-    // Task 2: Variables for Strings
-    var favoriteFood = "Pizza";
-    console.log("My favorite food is: " + favoriteFood);
-
-    // Task 3: Variables for Numbers
-    var num1 = 10;
-    var num2 = 5; 
-    console.log("Addition: " + (num1 + num2));
-    console.log("Subtraction: " + (num1 - num2));
-    console.log("Multiplication: " + (num1 * num2));
-    console.log("Division: " + (num1 / num2));
-
-    // Task 4: Variable Names Legal and Illegal
+// Task 3: Variables for Numbers
+var num1 = 10;
+var num2 = 5; 
+console.log("Addition: " + (num1 + num2));
+console.log("Subtraction: " + (num1 - num2));
+console.log("Multiplication: " + (num1 * num2));
+console.log("Division: " + (num1 / num2));
+// Task 4: Variable Names Legal and Illegal
     // Legal Variable Names
     var legalVariable = "This is legal";
 
+  
     var result = ((num1 + num2) * num1) / (num2 - 1);
-    console.log("Result using BODMAS: " + result);
+        console.log("Result using BODMAS: " + result);
 
-    var remainder = num1 % num2;
-    console.log("Remainder of " + num1 + " divided by " + num2 + " is: " + remainder);
-    var x = 5;
-    var postIncrement = x++;
-    console.log("Post Increment: " + postIncrement); // Outputs 5
-    
+
+        var remainder = num1 % num2;
+        console.log("Remainder of " + num1 + " divided by " + num2 + " is: " + remainder);
+        var x = 5;
+        var postIncrement = x++;
+        console.log("Post Increment: " + postIncrement); // Outputs 5
+        
     var y = 10;
     var preIncrement = ++y;
     console.log("Pre Increment: " + preIncrement); // Outputs 11
-    var firstName = "John";
-    var lastName = "Doe";
+    var firstName = "ubaid";
+    var lastName = "aqeel";
     var fullName = firstName + " " + lastName;
     console.log("Full Name: " + fullName);
     var userAge = prompt("Enter your age:");
@@ -55,4 +54,3 @@ function runJavaScriptCode() {
     } else {
         output.innerHTML = "Your grade is F.";
     }
-}
